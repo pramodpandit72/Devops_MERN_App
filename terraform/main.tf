f554f8ca9cd7284012496914645b17c7f2603cd2
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-05-10 17:29:39 +0530

    Add Ansible playbooks and roles for server configuration, Docker, and Kubernetes setup; update Dockerfile to use Node.js 20; modify Terraform main.tf for key name configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,15 @@ module "ec2" {
   vpc_id          = module.vpc.vpc_id
   subnet_id       = module.vpc.public_subnet_id
   instance_type   = var.instance_type
-  key_name        = var.key_name
+  key_name        = "Devops-MERN-App"
 }
 
+
+/*
 # Optional (advanced)
 module "eks" {
   source = "./modules/eks"
   vpc_id = module.vpc.vpc_id
   subnet_ids = [module.vpc.public_subnet_id]
 }
+*/
